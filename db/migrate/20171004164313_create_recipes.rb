8c3c7293b63193530c89c6db502c46ef1ca05e03
mattgallegos <mattgallegos88@gmail.com>
2017-10-04 10:54:22 -0600

    'Initial'

---
+++ db/migrate/20171004164313_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :image
+      t.text :ingredients
+      t.text :directions
+
+      t.timestamps
+    end
+  end
+end
